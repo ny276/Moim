18d27d2dcd05fa87c75befcf4f766946b1bb758d
ny276 <ny276@naver.com>
2018-05-21 17:17:32 +0900

    add MOIM

---
+++ app/src/main/java/moim/moim/Home.java
@@ -0,0 +1,20 @@
+package moim.moim;
+
+/**
+ * Created by D7701 on 2018-05-21.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Home extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.home, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/moim/moim/New.java
@@ -0,0 +1,20 @@
+package moim.moim;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by D7701 on 2018-05-21.
+ */
+
+public class New extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_page2, container, false);
+        return rootView;
+    }
+}
